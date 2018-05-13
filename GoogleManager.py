5d3598bf7841dbd4745f856c9bf2334117abdd56
cinekpl97 <cinekpl97@gmail.com>
2018-05-13 21:15:54 +0200

    Added: os, io lib file upload and download functions, but everything needs to be added manually
    changed scopes to access not only read, but also write

--- GoogleManager.py
+++ GoogleManager.py
@@ -8,19 +8,24 @@ from __future__ import print_function
 from googleapiclient.discovery import build
 from httplib2 import Http
 from oauth2client import file, client, tools
+from googleapiclient.http import MediaFileUpload, MediaIoBaseDownload
+import os, io
+
 
 # Setup the Drive v3 API
 
 
 def authorization():
-    SCOPES = 'https://www.googleapis.com/auth/drive.metadata.readonly'
+    scopes = 'https://www.googleapis.com/auth/drive'
     store = file.Storage('credentials.json')
     creds = store.get()
     if not creds or creds.invalid:
-        flow = client.flow_from_clientsecrets('client_secret.json', SCOPES)
+        flow = client.flow_from_clientsecrets('client_secret.json', scopes)
         creds = tools.run_flow(flow, store)
     service = build('drive', 'v3', http=creds.authorize(Http()))
     return service
+
+
 # Call the Drive v3 API
 
 
@@ -34,6 +39,26 @@ def files_list(listsize, service):
         print('Files:')
         for item in items:
             print('{0} ({1})'.format(item['name'], item['id']))
+
+
+def files_upload(filename, path, mimetype):
+    file_metadata = {'name': filename}
+    media = MediaFileUpload(path, mimetype=mimetype)
+    file = service.files().create(body=file_metadata, media_body=media, fields='id').execute()
+    print('File ID: %s' % file.get('id'))
+
+
+def files_download(file_id, pathtosave):
+    request = service.files().get_media(fileId=file_id)
+    fh = io.BytesIO()
+    downloader = MediaIoBaseDownload(fh, request)
+    done = False
+    while done is False:
+        status, done = downloader.next_chunk()
+        print("Download %d%%." % int(status.progress() * 100))
+    with io.open(pathtosave, 'wb') as f:
+        fh.seek(0)
+        f.write(fh.read())
 
 
 service = authorization()
