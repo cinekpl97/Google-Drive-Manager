48c58ddd1467ab78ce3ca02abbf481c90ea281b3
cinekpl97 <cinekpl97@gmail.com>
2018-06-05 19:02:44 +0200

    Found some exceptions making error, may be some left

--- Menu.py
+++ Menu.py
@@ -17,7 +17,9 @@ class Menu:
               "3. Download file\n"
               "4. Search for a File\n"
               "5. Send all files from SYNC folder\n"
-              "If you want to finish working just write EXIT")
+              "If you want to finish working just write 'exit'\n"
+              "If you want to clear screen, write 'cls'\n"
+              "If you want to see instructions write 'help'\n")
 
     @staticmethod
     def userchoice(drive, service):
@@ -27,30 +29,41 @@ class Menu:
             if choice == '1':
                 print("Write the amount of last added files you want to see: ")
                 amountoffiles = input("> ")
-                itemlist = drive.files_list(amountoffiles, service)
+                if amountoffiles.isdigit():
+                    itemlist = drive.files_list(amountoffiles, service)
+
             elif choice == '2':
                 window = Tk()
                 window.filename = filedialog.askopenfilename(initialdir='/', title="Select document",
                                                              filetypes=(("All Files", "*.*"), ("Text Files", "*.txt")))
                 path = window.filename
-                filename = ntpath.basename(path)
-                mime = mimetypes.guess_type(path)
-                mimetype = mime[0]
-                drive.files_upload(filename, path, mimetype)
+                if path == '':
+                    print("no file chosen")
+                else:
+                    filename = ntpath.basename(path)
+                    mime = mimetypes.guess_type(path)
+                    mimetype = mime[0]
+                    drive.files_upload(filename, path, mimetype)
+
             elif choice == '3':
                 print("From the last chosen by you list choose the file number you want to download")
                 file_number = int(input("> "))
-                googlefiletype = "vnd.google"
-                if googlefiletype in itemlist[file_number]['mimeType']:
-                    print("You can't download Google file, sir")
+                if len(itemlist) == 0:
+                    print("You haven't seen any files list yet")
                 else:
-                    chosenid = itemlist[file_number]['id']
-                    chosenname = itemlist[file_number]['name']
-                    drive.files_download(chosenid, chosenname)
+                    googlefiletype = "vnd.google"
+                    if googlefiletype in itemlist[file_number]['mimeType']:
+                        print("You can't download Google file, sir")
+                    else:
+                        chosenid = itemlist[file_number]['id']
+                        chosenname = itemlist[file_number]['name']
+                        drive.files_download(chosenid, chosenname)
+
             elif choice == '4':
                 print("choose by what phrase do you want to find the file")
                 phrase = input("> ")
                 itemlist = drive.files_search(100, phrase)
+
             elif choice == '5':
                 pathsync = r"D:\\PROGRAMOWANIE\\Python programy\\GoogleManager\\SYNC\\"
                 listfiles = os.listdir(pathsync)
@@ -65,4 +78,6 @@ class Menu:
                         drive.files_upload(filenamesync, temp_path, mimetypesync)
             elif choice == "cls":
                 os.system("cls")
+            elif choice == "help":
+                Menu.printoptions()
             choice = input("> ")
